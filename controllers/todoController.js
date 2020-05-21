4afca3783e9d635e7cbe51940595eb9814b8c3ca
Bart Kalanski <bkalanski@gmail.com>
2020-05-21 14:56:53 +0100

    added post and delete functionality

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,17 +1,29 @@
+//Loads body parser 
+var bodyParser = require('body-parser');
+//Dummy data 
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 //Sets up request handlers
 module.exports = function(app){
 
 //Handles get requests()
 app.get('/todo', function(req, res){
-    res.render('todo');
+    res.render('todo', {todos: data});
 
 });
 //Handles post requests(when user adds something to the list)
-app.post('/todo', function(req, res){
+app.post('/todo', urlencodedParser, function(req, res){
+data.push(req.body);
+res.json(data);
 
 });
 //Handles delete requests(allows user to delete items of the list).
-app.delete('/todo', function(req, res){
+app.delete('/todo/:item', function(req, res){
+    data = data.filter(function(todo){
+        return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
 
 });
 };
