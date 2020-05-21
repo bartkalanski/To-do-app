f968ad510fa955a5a5b27e02fccd4badc46df85e
Bart Kalanski <bkalanski@gmail.com>
2020-05-21 11:22:50 +0100

    set up the application using mvc

---
+++ controllers/todoController.js
@@ -0,0 +1,16 @@
+//Sets up request handlers
+module.exports = function(app){
+
+//Handles get requests()
+app.get('/todo', function(req, res){
+
+});
+//Handles post requests(when user adds something to the list)
+app.post('/todo', function(req, res){
+
+});
+//Handles delete requests(allows user to delete items of the list).
+app.delete('/todo', function(req, res){
+
+});
+};
